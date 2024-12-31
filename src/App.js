df7d5819e84e5408a9a96daa7870f0c887a03030
bluedawn123 <haemilyjh@naver.com>
2024-12-31 16:19:22 +0900

    커밋 테스트

--- src/App.js
+++ src/App.js
@@ -47,6 +47,8 @@ function Card(props){
       <img src={'https://codingapple1.github.io/shop/shoes' + (props.i +1) + '.jpg'} width="80%" />
       <h4>{ props.shoes.title }</h4>
       <p>{ props.shoes.price }</p>
+      <p>{ props.shoes.price }</p>
+
     </div>
   )
 }
